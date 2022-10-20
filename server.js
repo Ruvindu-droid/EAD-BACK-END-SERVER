6fb78c0def303f20aa980f1a85a94eab9c89e8b2
it19062884Ruvindu <it19062884@my.sliit.lk>
2022-10-20 23:52:40 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+//Add New Routes Below the Following >>>>>>>>>>>>>>>>
+const SiteRoute = require('./src/routes/route.site');
+
+
+
+dotenv.config();
+const app = express();
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+/**
+ * Get MONGODB_URI from .env
+ */
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, (error) => {
+    if (error) {
+        console.log('Database Error:', error.message);
+        console.log('######################################################');
+    }
+});
+
+mongoose.connection.once('open', () => {
+    console.log('Database Connected and EAD Project API on Progress');
+    console.log('######################################################');
+});
+
+app.route('/').get((req, res) => {
+    res.send('Our EAD Project Backend Nodejs API Called Successfully - R.K');
+
+  });
+
+
+app.use('/site', SiteRoute());
+
+app.listen(PORT, () => {
+    console.log('######################################################');
+    console.log(`Server is ON and running on PORT : ${PORT}`);
+    console.log('...Wait DB connecting...');
+});
