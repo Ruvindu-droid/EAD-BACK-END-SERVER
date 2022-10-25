20a74d9bd34803c9cb72fcbc6b9e8e5515337849
GWAI 2018 <73905758+GWAI2018@users.noreply.github.com>
2022-10-25 20:36:31 +0530

    Update function added

--- src/routes/route.station.js
+++ src/routes/route.station.js
@@ -11,6 +11,8 @@ module.exports = function () {
     router.post('/decrment-r-queue-by-name', controller_station.stationdecreaseQAmountRByname);
     router.post('/decrment-r-queue-by-special-function', controller_station.StationQdecreaseSpecialFunction);
     router.post('/increment-r-queue-by-special-function', controller_station.StationQincreaseSpecialFunction);
+     //update function
+    router.post('/update-station-by-name/:id', controller_station.updateDetailsById);
 
     return router;
 }
